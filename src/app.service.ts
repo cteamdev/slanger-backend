55b09fa845b53f179727f9e6d450182430617300
ItzNeviKat <nikitakoschelenko@gmail.com>
2021-10-12 20:30:44 +0300

    fix: Ловля необработанных ошибок

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,22 @@
-import { OnModuleInit } from '@nestjs/common';
+import { Logger, OnModuleInit } from '@nestjs/common';
 import { InjectMeiliSearch } from 'nestjs-meilisearch';
 import { MeiliSearch, Index } from 'meilisearch';
 
 import { Slang } from '@/slangs/entities/slang.entity';
 
 export class AppService implements OnModuleInit {
+  private readonly logger: Logger = new Logger(AppService.name);
   private readonly meiliIndex: Index<Slang> =
     this.meiliSearch.index<Slang>('slangs');
 
   constructor(@InjectMeiliSearch() private readonly meiliSearch: MeiliSearch) {}
 
   async onModuleInit(): Promise<void> {
+    // Ловим необработанные ошибки
+    process.on('unhandledRejection', (reason) => {
+      this.logger.error(reason);
+    });
+
     // Настройка MeiliSearch
     await this.meiliIndex.updateSettings({
       filterableAttributes: ['status'],
